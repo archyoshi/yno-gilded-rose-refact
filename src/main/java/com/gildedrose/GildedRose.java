1757086f8cd758f5ab5dd48173cb0ecebacd39c1
Thami Inaflas <inaflas.thami@gmail.com>
2018-05-12 20:30:40 +0200

    refact(GildedRose) used switch case on each item.name

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,10 +1,12 @@
 package com.gildedrose;
 
 class GildedRose {
+    private static final int MAX_QUALITY = 50;
+    private static final int QUALITY_FIRST_CAP = 10;
+    private static final int QUALITY_SECOND_CAP = 5;
     private static final String AGED_BRIE = "Aged Brie";
-    private static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
-    private static final String BACKSTAGE_PASSES_TO_A_TAFKAL80_ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
-
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    private static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
     final Item[] items;
 
     public GildedRose(Item[] items) {
@@ -13,61 +15,61 @@ class GildedRose {
 
     void updateQuality() {
         for (Item item : items) {
-            if (item.name.equals(AGED_BRIE)) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            } else if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80_ETC_CONCERT)) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+            switch (item.name) {
 
-                    if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80_ETC_CONCERT)) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
+                case SULFURAS:
+                    break;
 
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
+                case AGED_BRIE:
+                    increaseQualityOf(item);
+                    decreaseSellInOf(item);
+                    if (item.sellIn < 0) {
+                        increaseQualityOf(item);
                     }
-                }
-            } else {
-                if (item.quality > 0) {
-                    if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                        decreaseQualityOf(item);
+                    break;
+
+                case BACKSTAGE:
+                    increaseQualityOf(item);
+                    if (item.sellIn <= QUALITY_FIRST_CAP) {
+                        increaseQualityOf(item);
                     }
-                }
-            }
 
-            if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                item.sellIn = item.sellIn - 1;
-            }
+                    if (item.sellIn <= QUALITY_SECOND_CAP) {
+                        increaseQualityOf(item);
+                    }
 
-            if (item.sellIn < 0) {
-                if (!item.name.equals(AGED_BRIE)) {
-                    if (!item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80_ETC_CONCERT)) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                                decreaseQualityOf(item);
-                            }
-                        }
-                    } else {
+                    decreaseSellInOf(item);
+
+                    if (item.sellIn < 0) {
                         item.quality = 0;
                     }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
+                    break;
+
+                default:
+                    decreaseQualityOf(item);
+                    decreaseSellInOf(item);
+                    if (item.sellIn < 0) {
+                        decreaseQualityOf(item);
                     }
-                }
+                    break;
             }
         }
     }
 
-    private void decreaseQualityOf(Item item) {
-        item.quality -= 1;
+    private void decreaseSellInOf(Item item) {
+        item.sellIn -= 1;
     }
+
+    private void increaseQualityOf(Item item) {
+        if (item.quality < MAX_QUALITY) {
+            item.quality += 1;
+        }
+    }
+
+    private void decreaseQualityOf(Item item) {
+        if (item.quality > 0) {
+            item.quality -= 1;
+        }
+    }
+
 }
